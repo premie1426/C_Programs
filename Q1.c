7a4ec7f110e8d31769b6c581e52b44962f400811
Pritham Prashanth <prithamprashanth14@gmail.com>
2025-05-27 13:54:13 +0530

    initial commit

---
+++ Q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+
+int main(){
+    int a,b,c,sum,prime=1;
+
+    printf("Enter the three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    sum = a + b + c;
+    printf("The sum is: %d \n", sum);
+
+    if (sum % 2 == 0){
+        printf("The sum is even.\n");}
+    else{
+        printf("The sum is odd.\n");}
+
+    if (sum <= 1) {
+        printf("The number is neither prime nor composite.\n");
+    } else {
+        for (int i = 2; i * i <= sum; i++) {
+            if (sum % i == 0) {
+                prime = 0;
+                break;
+            }
+        } 
+    }
+
+    if (prime){
+        printf("The number is prime.\n");}
+    else{
+        printf("The number is composite.\n");}
+    
+}
