273096c7288a371239a6f06f47fd62ab5d9b6e46
Pritham Prashanth <prithamprashanth14@gmail.com>
2025-05-27 20:35:30 +0530

    q2

--- Q2.c
+++ Q2.c
@@ -1,9 +1,14 @@
 #include <stdio.h> 
 
 int main(){
-    int n;
+    int n,hours,min,sec;
     printf("Enter seconds: ");
-    scanf("$d",&n);
+    scanf("%d",&n);
 
-    
+    hours = n / 3600;
+    min = (n % 3600) / 60;
+    sec = (n % 3600) % 60;
+
+    printf("%d hours, %d minutes and %d seconds\n", hours, min, sec);
+
 }
