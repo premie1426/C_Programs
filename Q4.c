d07cf51ca99e4adf7557d0095158a16808bd4230
Pritham Prashanth <prithamprashanth14@gmail.com>
2025-05-28 16:58:17 +0530

    Q4

--- Q4.c
+++ Q4.c
@@ -1,5 +1,23 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-    
+int main() {
+    char password[] = "password";
+    char input[50];
+    int attempts = 3;
+
+    while (attempts > 0) {
+        printf("Enter password: ");
+        scanf("%s", input);
+
+        if (strcmp(input, password) == 0) {
+            printf("Login successful!\n");
+            return 0;
+        } else {
+            attempts--;
+            printf("Incorrect password. Attempts left: %d\n", attempts);
+        }
+    }
+
+    printf("Login failed\n");
 }
