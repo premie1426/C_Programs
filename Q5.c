c3105409896ccedabd460ef0a05df09e8977f9be
Pritham Prashanth <prithamprashanth14@gmail.com>
2025-05-29 20:20:46 +0530

    Q5

--- Q5.c
+++ Q5.c
@@ -8,8 +8,27 @@ int main(){
     for (int i = 0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("The entered array is: ");
+    printf("The entered array is: \n");
     for(int j = 0;j<n;j++){
         printf("%d ",arr[j]);
     }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; ) {
+            if (arr[i] == arr[j]) {
+                for (int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    printf("Array after removing duplicates:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
